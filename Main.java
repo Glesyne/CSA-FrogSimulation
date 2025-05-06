d751812e01e49255a207017edd748ebed8dcdc0a
Glesyne <alexc161@nycstudents.net>
2025-05-06 02:33:53 +0000

    Final Version

--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
-
+public class Main{
+    public static void main(String[] args) {
+        FrogSimulation a = new FrogSimulation(24, 5); 
+        //System.out.println(a.simulate());
+        System.out.println(a.runSimulations(4));
+    }
+}
